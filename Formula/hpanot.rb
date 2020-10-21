c1931e6253cd4f0517b5db838a49db1c308bac75
Alex Balgavy <alex@balgavy.eu>
2020-10-21 13:23:56 +0200

    hpanot v1.0.1

--- Formula/hpanot.rb
+++ Formula/hpanot.rb
@@ -1,8 +1,8 @@
 class Hpanot < Formula
   desc "get your annotations in Markdown from hypothes.is"
   homepage "https://github.com/thezeroalpha/hpanot"
-  url "https://github.com/thezeroalpha/hpanot/archive/v1.0.tar.gz"
-  sha256 "e8b6605ad0aa9a723a22367bac5ac2c08f857062f9ad1bfd079def03007dd0e2"
+  url "https://github.com/thezeroalpha/hpanot/archive/v1.0.1.tar.gz"
+  sha256 "1854b1a7c488c9e606f2d8a13b4ea35ffc6c7f9499c9523f77a300229efe2665"
 
   uses_from_macos "ruby" => "2.7"
 
