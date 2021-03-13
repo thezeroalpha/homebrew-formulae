9bdc091b7962f819ac419b5f6fb94a07a3725635
Alex Balgavy <alex@balgavy.eu>
2021-03-13 18:53:15 +0100

    vbox v1.0.1

--- Formula/vbox.rb
+++ Formula/vbox.rb
@@ -1,17 +1,17 @@
 class Vbox < Formula
   desc "vbox lets you manipulate (start, stop, pause, suspend) VirtualBox VMs, and change shared folders."
   homepage "https://git.alex.balgavy.eu/vbox"
-  url "https://github.com/thezeroalpha/vbox/archive/v1.0.tar.gz"
-  sha256 "557a76ceb4fa4f1b61d9231cd12c39fca337659a799c2aefb257334364cc5d23"
+  url "https://github.com/thezeroalpha/vbox/archive/v1.0.1.tar.gz"
+  sha256 "c8981ac89a51fd33885241bfc859919c21cc2f69c00591154d3ddd7becab9d76"
 
   def install
     bin.install "vbox"
     man1.install "vbox.1.man" => "vbox.1"
-    zsh_completion.install "_vbox"
+    zsh_completion.install "vbox.zsh" => "_vbox"
   end
 
   test do
-    # TODO need a better test
-    true
+    output = shell_output("#{bin}/vbox -h")
+    assert_include output, "list, ls"
   end
 end
