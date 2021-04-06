877b7a3e71afa4a8f6e8420d77ccc2df515477f4
Alex Balgavy <alex@balgavy.eu>
2021-04-06 14:45:18 +0200

    gocket v0.2.4

--- Formula/gocket.rb
+++ Formula/gocket.rb
@@ -1,9 +1,9 @@
 class Gocket < Formula
   desc "CLI / TUI for Pocket"
   homepage "https://github.com/Phantas0s/gocket"
-  url "https://github.com/Phantas0s/gocket/releases/download/v0.2.4/gocket_0.2.4_Darwin_x86_64.tar.gz"
-  sha256 "552f8654f29066488bd6f6ca7d89f477f19aa545ed81d7f94747c10c6512034c"
   version "0.2.4"
+  url "https://github.com/Phantas0s/gocket/releases/download/v0.2.4/gocket_0.2.4_Darwin_x86_64.tar.gz"
+  sha256 "971594be7b9b0efef3e68665a6d2a43dff1ea6cb7665725ea2a5212b9035186f"
 
   def install
     bin.install "gocket"
