b60537264bbf44719a1c5a80b271ed36f93b4921
Alex Balgavy <alex@balgavy.eu>
2021-04-06 14:38:42 +0200

    radio v1.1.1

--- Formula/radio.rb
+++ Formula/radio.rb
@@ -1,8 +1,8 @@
 class Radio < Formula
   desc "Command-line internet radio player"
   homepage "https://github.com/thezeroalpha/radio"
-  url "https://github.com/thezeroalpha/radio/archive/v1.1.0.tar.gz"
-  sha256 "a33724932434b2b941510a9538980bca45c286a5434161c774292bdca9c403e5"
+  url "https://github.com/thezeroalpha/radio/archive/v1.1.1.tar.gz"
+  sha256 "57e36ff00ac8726dd53a7b550aa4aae5749521074205b97cda1bd0bfec7c72a7"
 
   uses_from_macos "ruby" => "2.7"
 
