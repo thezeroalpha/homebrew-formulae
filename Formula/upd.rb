e9704aa45db7f8fac83079160879993b0ebaed07
Alex Balgavy <alex@balgavy.eu>
2021-05-08 13:40:16 +0200

    upd v1.0.1

--- Formula/upd.rb
+++ Formula/upd.rb
@@ -1,8 +1,8 @@
 class Upd < Formula
   desc "upd - update all of your package systems on macOS at once"
   homepage "https://github.com/thezeroalpha/upd"
-  url "https://github.com/thezeroalpha/upd/archive/v1.0.tar.gz"
-  sha256 "2d0b707ded3dbd167b9001df8ffce62c992cad64964569f993c939c0a5797f72"
+  url "https://github.com/thezeroalpha/upd/archive/v1.0.1.tar.gz"
+  sha256 "ee2f84ff28149548628222c7d9331c69ff8aa8a6b7e512ff92661afdcbd511f8"
 
   uses_from_macos "ruby"
 
@@ -13,6 +13,6 @@ class Upd < Formula
 
   test do
     output = shell_output("#{bin}/upd -h")
-    assert_include output, "-n, --dry-run"
+    assert_includes output, "-n, --dry-run"
   end
 end
