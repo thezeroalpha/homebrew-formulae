460d7f216540254abad4fd99104e6b37c31f0f14
Alex Balgavy <alex@balgavy.eu>
2021-08-20 16:27:16 +0200

    New formula: choice (pedantic.software)

---
+++ Formula/choice.rb
@@ -0,0 +1,16 @@
+class Choice < Formula
+  desc "A small console based menu selector, inspired by dmenu	"
+  homepage "https://pedantic.software/git/choice"
+  head "https://pedantic.software/git/choice", using: :git
+  sha256 "e17bb79c6718184dcd7d69c23e262c044e275905560ce427e0fdd14a07b257fd"
+
+  def install
+    puts `pwd`
+    inreplace (buildpath/'Makefile'), 'CFLAGS ?= ', 'CFLAGS ?= -D_DARWIN_C_SOURCE '
+    system 'make', 'install', "PREFIX=#{prefix}"
+  end
+
+  test do
+    assert_includes shell_output("#{bin}/choice -h"), "Usage:"
+  end
+end
