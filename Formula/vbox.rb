758ed68e734f9a8899fa64803204d4a85b91426f
Alex Balgavy <alex@balgavy.eu>
2021-09-10 09:40:05 +0200

    vbox v1.0.2

--- Formula/vbox.rb
+++ Formula/vbox.rb
@@ -1,8 +1,8 @@
 class Vbox < Formula
   desc "vbox lets you manipulate (start, stop, pause, suspend) VirtualBox VMs, and change shared folders."
   homepage "https://git.alex.balgavy.eu/vbox"
-  url "https://github.com/thezeroalpha/vbox/archive/v1.0.1.tar.gz"
-  sha256 "c8981ac89a51fd33885241bfc859919c21cc2f69c00591154d3ddd7becab9d76"
+  url "https://github.com/thezeroalpha/vbox/archive/v1.0.2.tar.gz"
+  sha256 "c0301c89f686e6a08ef29c3390a9afc45523492216adf0656cceeba2b665905e"
 
   def install
     bin.install "vbox"
