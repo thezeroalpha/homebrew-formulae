1e41be72fe490574439958b0ecb1cdbf988bee30
Alex Balgavy <alex@balgavy.eu>
2024-01-04 22:32:25 +0100

    Alacritty v0.12.3: until I have time to switch my config

---
+++ Casks/alacritty@0.12.3.rb
@@ -0,0 +1,34 @@
+cask "alacritty@0.12.3" do
+  version "0.12.3"
+  sha256 "7fc3220a3ad93ab2e555d2a9724506fc314ca4beaa81ce16e43cb3e9c06f1a93"
+
+  url "https://github.com/alacritty/alacritty/releases/download/v#{version}/Alacritty-v#{version}.dmg"
+  name "Alacritty"
+  desc "GPU-accelerated terminal emulator"
+  homepage "https://github.com/alacritty/alacritty/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Alacritty.app"
+  binary "Alacritty.app/Contents/MacOS/alacritty"
+  binary "Alacritty.app/Contents/Resources/completions/_alacritty",
+         target: "#{HOMEBREW_PREFIX}/share/zsh/site-functions/_alacritty"
+  binary "Alacritty.app/Contents/Resources/completions/alacritty.bash",
+         target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/alacritty"
+  binary "Alacritty.app/Contents/Resources/completions/alacritty.fish",
+         target: "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/alacritty.fish"
+  binary "Alacritty.app/Contents/Resources/61/alacritty",
+         target: "#{ENV.fetch("TERMINFO", "~/.terminfo")}/61/alacritty"
+  binary "Alacritty.app/Contents/Resources/61/alacritty-direct",
+         target: "#{ENV.fetch("TERMINFO", "~/.terminfo")}/61/alacritty-direct"
+  manpage "Alacritty.app/Contents/Resources/alacritty.1.gz"
+  manpage "Alacritty.app/Contents/Resources/alacritty-msg.1.gz"
+
+  zap trash: [
+    "~/Library/Preferences/io.alacritty.plist",
+    "~/Library/Saved Application State/io.alacritty.savedState",
+  ]
+end
